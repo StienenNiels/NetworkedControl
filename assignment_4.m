e0324b2f94c5b4d1f0b8fcac0e65a83bf3ee74b9
nstienen <n.l.stienen@student.tudelft.nl>
2024-06-04 21:54:36 +0200

    Update assignment_4.m

--- assignment_4.m
+++ assignment_4.m
@@ -10,3 +10,15 @@ set(groot, 'defaultLegendInterpreter','latex');
 
 % state vector is (x,y,xdot,ydot)
 aircraft; % Load the given parameters
+
+% Create structures for each plane to make workspace more structured
+for i = 1:445
+    eval(sprintf('P%d.A = A%d;', i, i));
+    eval(sprintf('P%d.B = B%d;', i, i));
+    eval(sprintf('P%d.x0 = x0%d;', i, i));
+end
+clearvars -except P1 P2 P3 P4 Tfinal umax
+
+
+% Objective
+% Write function to generate prediction matrices for Np steps
